d00723a41ba2bd54578aeec95640a5140931eea5
okoid721 <okoid721@gmail.com>
2024-03-13 23:17:17 +0100

    portfolio

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -1,42 +1,40 @@
-import Link from 'next/link';
+import React from 'react';
 import Containers from '../Containers';
-import { Redressed } from 'next/font/google';
+import Link from 'next/link';
+import { Shadows_Into_Light } from 'next/font/google';
 import CartCount from './CartCount';
 import UserMenu from './UserMenu';
 import { getCurrentUser } from '@/actions/getCurrentUser';
+import Categories from './Categories';
+import SearchBar from './SearchBar';
 
-const redressed = Redressed({ subsets: ['latin'], weight: ['400'] });
+const redressed = Shadows_Into_Light({ subsets: ['latin'], weight: ['400'] });
 
 const NavBar = async () => {
   const currentUser = await getCurrentUser();
 
   return (
-    <div
-      className="
-    sticky top-0 w-full bg-[#ffff] z-30 shadow-smooth
-     text-[#66666]"
-    >
-      <div className=" p-4 border-b-[1px] ">
+    <div className="sticky top-0 w-full bg-[#131921] select-none z-30 shadow-sm">
+      <div className="py-4 border-b-[1px]">
         <Containers>
-          <div
-            className="
-                flex items-center justify-between gap-3 md:gap-0
-            "
-          >
+          <div className="flex items-center justify-between gap-3 md:gap-0">
             <Link
               href="/"
-              className={`${redressed.className} font-bold text-2xl `}
+              className={`${redressed.className} text-3xl md:text-5xl font-bold text-[#F9A024]`}
             >
-              Kings Mobile
+              Kings-store.io
             </Link>
-            <div className=" hidden md:block">Search</div>
-            <div className=" flex items-center gap-8 md:gap-12">
+            <div className="hidden md:block">
+              <SearchBar />
+            </div>
+            <div className="flex items-center gap-8 md:gap-12">
               <CartCount />
               <UserMenu currentUser={currentUser} />
             </div>
           </div>
         </Containers>
       </div>
+      <Categories />
     </div>
   );
 };
